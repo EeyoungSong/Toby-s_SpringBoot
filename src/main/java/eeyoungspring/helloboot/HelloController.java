9a9776b75db3a5c324af44fa7443311491928130
eeyoungsong@gmail.com <eeyoungsong@gmail.com>
2024-05-06 23:07:58 +0900

    simple hello service 의존관계 생성

--- src/main/java/eeyoungspring/helloboot/HelloController.java
+++ src/main/java/eeyoungspring/helloboot/HelloController.java
@@ -3,9 +3,12 @@ package eeyoungspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name)); // 파라미터가 null인지 아닌지 체크
     }
 }
---
+++ src/main/java/eeyoungspring/helloboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package eeyoungspring.helloboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello" + name;
+    }
+}
