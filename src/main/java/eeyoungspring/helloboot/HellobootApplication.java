f5db7c7456becfb270969a97d6a0322dfa8b1876
eeyoungsong@gmail.com <eeyoungsong@gmail.com>
2024-05-06 21:42:32 +0900

    로직 분리

--- src/main/java/eeyoungspring/helloboot/HellobootApplication.java
+++ src/main/java/eeyoungspring/helloboot/HellobootApplication.java
@@ -23,17 +23,20 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // Tomcat 서블릿 컨테이너 띄우기
 		WebServer webServer = serverFactory.getWebServer(servletContext -> { // 매소드가 하나인 FunctionalInterface이기 때문에 람다식으로 대체 가능, servletContext 라는 파라미터 받기
-         	servletContext.addServlet("frontcontroller", new HttpServlet() { // 익명클래스 사용
+         	HelloController helloController = new HelloController();
+			servletContext.addServlet("frontcontroller", new HttpServlet() { // 익명클래스 사용
 
 				 @Override
-				 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					 // frontcontroller 등록하는 코드 - 모든 요청에 대해서 매핑과 공통 코드 처리 , 각 로직은 오브젝트로 분리
+				 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {// frontcontroller 등록하는 코드
+					 // 모든 요청에 대해서 매핑과 공통 코드
 					 if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						 String name = req.getParameter("name"); // request parameter 받을 수 있음
 
+						 String ret = helloController.hello(name); // 로직 분리 & 바인딩
+
 						 resp.setStatus(HttpStatus.OK.value());
 						 resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // text 그냥 입력하는 건 오타의 위험이 있음 -> 미리 정의된 enum 활용
-						 resp.getWriter().println("Hello " + name);
+						 resp.getWriter().println(ret);
 					 }
 					 else if (req.getRequestURI().equals("/user")) {
 						 //
