1589f0eeda7b0c6a1a8300b6e07226715ba10949
eeyoungsong@gmail.com <eeyoungsong@gmail.com>
2024-05-06 21:34:48 +0900

    set frontcontroller

--- src/main/java/eeyoungspring/helloboot/HellobootApplication.java
+++ src/main/java/eeyoungspring/helloboot/HellobootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -22,16 +23,26 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // Tomcat 서블릿 컨테이너 띄우기
 		WebServer webServer = serverFactory.getWebServer(servletContext -> { // 매소드가 하나인 FunctionalInterface이기 때문에 람다식으로 대체 가능, servletContext 라는 파라미터 받기
-         	servletContext.addServlet("hello", new HttpServlet() { // 익명클래스 사용
-				 @Override
-				 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { // servlet 등록하는 코드
-					 String name = req.getParameter("name"); // request parameter 받을 수 있음
+         	servletContext.addServlet("frontcontroller", new HttpServlet() { // 익명클래스 사용
 
-					 resp.setStatus(HttpStatus.OK.value());
-					 resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // text 그냥 입력하는 건 오타의 위험이 있음 -> 미리 정의된 enum 활용
-					 resp.getWriter().println("Hello " + name);
+				 @Override
+				 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					 // frontcontroller 등록하는 코드 - 모든 요청에 대해서 매핑과 공통 코드 처리 , 각 로직은 오브젝트로 분리
+					 if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						 String name = req.getParameter("name"); // request parameter 받을 수 있음
+
+						 resp.setStatus(HttpStatus.OK.value());
+						 resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // text 그냥 입력하는 건 오타의 위험이 있음 -> 미리 정의된 enum 활용
+						 resp.getWriter().println("Hello " + name);
+					 }
+					 else if (req.getRequestURI().equals("/user")) {
+						 //
+					 }
+					 else {
+						 resp.setStatus(HttpStatus.NOT_FOUND.value());
+					 }
 				 }
-			}).addMapping("/hello"); // 요청마다 서블릿 하나씩 매핑
+			}).addMapping("/*"); // 모든 요청에 대하여
         });
 		webServer.start();
 	}
